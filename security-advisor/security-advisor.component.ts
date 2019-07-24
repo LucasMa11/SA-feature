5cbb7185a99679193072a7aa741c3cc45af63096
Gian Zheng <zhengxp3@lenovo.com>
2019-07-24 15:16:11 +0800

    update beta security advisor component

--- security-advisor/security-advisor.component.ts
+++ security-advisor/security-advisor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { VantageShellService } from '../../../services/vantage-shell.service';
+import { SecurityAdvisor, PasswordManager, Vpn, WindowsHello } from '@lenovo/tan-client-bridge';
 
 @Component({
   selector: 'app-security-advisor',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecurityAdvisorComponent implements OnInit {
 
-  constructor() { }
+  securityAdvisor: SecurityAdvisor;
+  password: PasswordManager;
+  vpn: Vpn;
+  windowsHello: WindowsHello;
+
+  constructor(vantageShellService: VantageShellService) {
+    this.securityAdvisor = vantageShellService.getSecurityAdvisor();
+    if (this.securityAdvisor) {
+      this.password = this.securityAdvisor.passwordManager;
+      this.vpn = this.securityAdvisor.vpn;
+      this.windowsHello = this.securityAdvisor.windowsHello;
+    }
+  }
 
   ngOnInit() {
+    this.securityAdvisor.refresh();
   }
 
 }
